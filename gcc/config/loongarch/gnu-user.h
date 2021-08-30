17d5d4feb788b83488b1fa70c1dcaf1a4d21d601
WANG Xuerui <git@xen0n.name>
2021-08-30 16:43:52 +0800

    LoongArch: fix build when paired with glibc-2.34 without separate libpthread

--- gcc/config/loongarch/gnu-user.h
+++ gcc/config/loongarch/gnu-user.h
@@ -76,7 +76,9 @@ along with GCC; see the file COPYING3.  If not see
 #undef ASM_OUTPUT_REG_POP
 
 #undef LIB_SPEC
-#define LIB_SPEC GNU_USER_TARGET_LIB_SPEC
+/* LoongArch support in glibc comes after the libc-libpthread merger, so
+   LoongArch systems never had separate libpthread.  */
+#define LIB_SPEC GNU_USER_TARGET_NO_PTHREADS_LIB_SPEC
 
 #define NO_SHARED_SPECS ""
 
